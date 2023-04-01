db6cfdca64c2b277216540ef466c57f8bad202e9
74C17N3P7UN3 <leocalzavara05@gmail.com>
2023-04-01 10:40:16 +0200

    Test commit

--- src/com/supermario/main/Main.java
+++ src/com/supermario/main/Main.java
@@ -2,6 +2,8 @@ package com.supermario.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Leo!");
+        System.out.println("Hello, Lima!");
+        System.out.println("Hello, Miche!");
     }
 }
