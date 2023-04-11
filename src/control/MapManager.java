b67b441a7212d5428d66d321bdcbeb5162877003
74C17N3P7UN3 <leocalzavara05@gmail.com>
2023-04-11 09:59:30 +0200

    Basic implementation of MapManager

---
+++ src/control/MapManager.java
@@ -0,0 +1,33 @@
+package control;
+
+import model.Map;
+import view.ImageLoader;
+
+/**
+ * Creates and handles all the game-level collisions and
+ * behaviours. It provides checks and low-level conditions
+ * for working in sync with the {@link GameEngine}.
+ *
+ * @author TacitNeptune
+ * @version 0.1.0
+ */
+public class MapManager {
+    private Map map;
+
+    public MapManager() {}
+
+    /**
+     * Creates the map using the {@link MapCreator}.
+     *
+     * @param imageLoader The loader responsible for
+     *                    drawing the map in the creator.
+     * @param mapName The name of the map to be loaded.
+     * @return If the map was loaded successfully.
+     */
+    public boolean createMap(ImageLoader imageLoader, String mapName) {
+        MapCreator mapCreator = new MapCreator(imageLoader);
+        map = mapCreator.createMap(mapName);
+
+        return map != null;
+    }
+}
