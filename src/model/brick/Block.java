cfe08cd5bf3da5032458415345ddc51cef4977d3
74C17N3P7UN3 <leocalzavara05@gmail.com>
2023-04-12 09:06:11 +0200

    Refactored Brick classes

--- src/model/brick/Block.java
+++ src/model/brick/Block.java
@@ -3,9 +3,7 @@ package model.brick;
 import java.awt.image.BufferedImage;
 
 public class Block extends Brick {
-
-	public Block(double x, double y, BufferedImage style) {
-		super(x, y, style);
-	}
-
+    public Block(double x, double y, BufferedImage style) {
+        super(x, y, style);
+    }
 }
