e0cae4d131c1fd622688a0fa84c50e3433cefbd8
74C17N3P7UN3 <leocalzavara05@gmail.com>
2023-04-12 22:28:24 +0200

    Code cleanup
    Fixed the sprite too as it had a white background and it wasn't transparent

---
+++ src/model/enemy/Koopa.java
@@ -0,0 +1,9 @@
+package model.enemy;
+
+import java.awt.image.BufferedImage;
+
+public class Koopa extends Enemy {
+    public Koopa(double x, double y, BufferedImage style) {
+        super(x, y, style);
+    }
+}
--- src/model/enemy/KoopaTroopa.java
+++
@@ -1,9 +0,0 @@
-package model.enemy;
-
-import java.awt.image.BufferedImage;
-
-public class KoopaTroopa extends Enemy {
-    public KoopaTroopa(double x, double y, BufferedImage style) {
-        super(x, y, style);
-    }
-}
